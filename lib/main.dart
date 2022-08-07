e19cb025007f846d564b8efc156b7af86b7df198
Ashutosh Rai <ashutosh.developer78@gmail.com>
2022-08-07 18:38:54 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:carousel_slider/carousel_slider.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Carousel Slider'),
+        centerTitle: true,
+      ),
+      body: Container(),
+    ));
+  }
+}
